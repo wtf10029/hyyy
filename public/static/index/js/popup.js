d308691af9fd9bd38fcfea78926fae5af6c2f0aa
king <569700663@qq.com>
2017-04-13 13:41:34 +0800

    补充

---
+++ public/static/index/js/popup.js
@@ -0,0 +1,120 @@
+//01
+function popup1(show_div) {
+	if (document.getElementById("showdiv01").style.display = 'block') {
+		document.getElementById("showdiv02").style.display = 'none';
+		document.getElementById("showdiv03").style.display = 'none';
+		document.getElementById("showdiv04").style.display = 'none';
+		document.getElementById("showdiv05").style.display = 'none';
+		document.getElementById("showdiv06").style.display = 'none';
+	}
+
+}
+
+function closediv1(show_div) {
+	document.getElementById("showdiv01").style.display = 'none';
+	document.getElementById("showdiv02").style.display = 'none';
+	document.getElementById("showdiv03").style.display = 'none';
+	document.getElementById("showdiv04").style.display = 'none';
+	document.getElementById("showdiv05").style.display = 'none';
+	document.getElementById("showdiv06").style.display = 'none';
+}
+
+
+//02
+function popup2(show_div) {
+	if (document.getElementById("showdiv02").style.display = 'block') {
+		document.getElementById("showdiv01").style.display = 'none';
+		document.getElementById("showdiv03").style.display = 'none';
+		document.getElementById("showdiv04").style.display = 'none';
+		document.getElementById("showdiv05").style.display = 'none';
+		document.getElementById("showdiv06").style.display = 'none';
+	}
+}
+
+function closediv2(show_div) {
+	document.getElementById("showdiv01").style.display = 'none';
+	document.getElementById("showdiv02").style.display = 'none';
+	document.getElementById("showdiv03").style.display = 'none';
+	document.getElementById("showdiv04").style.display = 'none';
+	document.getElementById("showdiv05").style.display = 'none';
+	document.getElementById("showdiv06").style.display = 'none';
+}
+
+//03
+function popup3(show_div) {
+	if (document.getElementById("showdiv03").style.display = 'block') {
+		document.getElementById("showdiv01").style.display = 'none';
+		document.getElementById("showdiv02").style.display = 'none';
+		document.getElementById("showdiv04").style.display = 'none';
+		document.getElementById("showdiv05").style.display = 'none';
+		document.getElementById("showdiv06").style.display = 'none';
+	}
+}
+
+function closediv3(show_div) {
+	document.getElementById("showdiv01").style.display = 'none';
+	document.getElementById("showdiv02").style.display = 'none';
+	document.getElementById("showdiv03").style.display = 'none';
+	document.getElementById("showdiv04").style.display = 'none';
+	document.getElementById("showdiv05").style.display = 'none';
+	document.getElementById("showdiv06").style.display = 'none';
+}
+
+//04
+function popup4(show_div) {
+	if (document.getElementById("showdiv04").style.display = 'block') {
+		document.getElementById("showdiv01").style.display = 'none';
+		document.getElementById("showdiv03").style.display = 'none';
+		document.getElementById("showdiv02").style.display = 'none';
+		document.getElementById("showdiv05").style.display = 'none';
+		document.getElementById("showdiv06").style.display = 'none';
+	}
+}
+
+function closediv4(show_div) {
+	document.getElementById("showdiv01").style.display = 'none';
+	document.getElementById("showdiv02").style.display = 'none';
+	document.getElementById("showdiv03").style.display = 'none';
+	document.getElementById("showdiv04").style.display = 'none';
+	document.getElementById("showdiv05").style.display = 'none';
+	document.getElementById("showdiv06").style.display = 'none';
+}
+//05
+function popup5(show_div) {
+	if (document.getElementById("showdiv05").style.display = 'block') {
+		document.getElementById("showdiv01").style.display = 'none';
+		document.getElementById("showdiv03").style.display = 'none';
+		document.getElementById("showdiv04").style.display = 'none';
+		document.getElementById("showdiv02").style.display = 'none';
+		document.getElementById("showdiv06").style.display = 'none';
+	}
+}
+
+function closediv5(show_div) {
+	document.getElementById("showdiv01").style.display = 'none';
+	document.getElementById("showdiv02").style.display = 'none';
+	document.getElementById("showdiv03").style.display = 'none';
+	document.getElementById("showdiv04").style.display = 'none';
+	document.getElementById("showdiv05").style.display = 'none';
+	document.getElementById("showdiv06").style.display = 'none';
+}
+
+//06
+function popup6(show_div) {
+	if (document.getElementById("showdiv06").style.display = 'block') {
+		document.getElementById("showdiv01").style.display = 'none';
+		document.getElementById("showdiv03").style.display = 'none';
+		document.getElementById("showdiv04").style.display = 'none';
+		document.getElementById("showdiv05").style.display = 'none';
+		document.getElementById("showdiv02").style.display = 'none';
+	}
+}
+
+function closediv6(show_div) {
+	document.getElementById("showdiv01").style.display = 'none';
+	document.getElementById("showdiv02").style.display = 'none';
+	document.getElementById("showdiv03").style.display = 'none';
+	document.getElementById("showdiv04").style.display = 'none';
+	document.getElementById("showdiv05").style.display = 'none';
+	document.getElementById("showdiv06").style.display = 'none';
+}
