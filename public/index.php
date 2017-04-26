c2c0f451307812e5ec7715b3ee7f8b17520cdfa2
king <569700663@qq.com>
2017-04-26 22:58:26 +0800

    修改入口文件

--- public/index.php
+++ public/index.php
@@ -14,7 +14,7 @@
 // 定义应用目录
 define('APP_PATH', __DIR__ . '/../application/');
 
-define('SITE_URL', 'http://www.huayumedicine.com');
+define('SITE_URL', 'http://localhost');
 
 // 加载框架引导文件
 require __DIR__ . '/../thinkphp/start.php';
